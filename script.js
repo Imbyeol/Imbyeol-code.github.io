ecfe8d5e1d9550478f20279f4fb795af50f81193
Imbyeol <139058150+Imbyeol@users.noreply.github.com>
2023-07-09 07:07:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let firstNumber=document.getElementById("firstNum");
+
+let secondNumber=document.getElementById("secondNum");
+
+let operator=document.getElementById("operators");
+
+let calculateBtn=document.getElementById("calculateBtn")
+
+let result=document.getElementById("result");
+
+calculateBtn.addEventListener("click",calculate);
+
+function calculate(){
+    
+    let answer;
+
+    let num1=firstNumber.value;
+    let num2=secondNumber.value;
+    if(operator.value==="+")
+    {
+        answer = parseFloat(num1)+parseFloat(num2);
+    }
+    else if(operator.value==="-"){
+        answer = parseFloat(num1)-parseFloat(num2);
+    }
+    else if(operator.value==="*"){
+        answer= parseFloat(num1)*parseFloat(num2);
+    }
+    else if(operator.value==="/"){
+        answer= parseFloat(num1)/parseFloat(num2);
+    }
+
+    result.innerText=answer;
+}
